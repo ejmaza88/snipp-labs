296e200803abe7d57d5350ef49578df9487563d8
ejmaza88 <esvin@fimed.com>
2021-09-30 10:38:02 -0500

    Config webpack.dev.js to accept local deb builds or to start the webpack dev server.

--- snipps/webpack.dev.js
+++ snipps/webpack.dev.js
@@ -5,7 +5,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 
-module.exports = merge(common, {
+// starts the webpack dev server
+const devServerConfig = {
     devServer: {
         // compress: true,
         // allowedHosts: ['*'],
@@ -13,13 +14,13 @@ module.exports = merge(common, {
         // host: '0.0.0.0',
         port: 7654,
         headers: { "Access-Control-Allow-Origin": "*" },
-        writeToDisk: (filePath) => /\.json$/.test(filePath),  // save a copy of the manifest JSON file to disk
+        writeToDisk: (filePath) => /\.json$/.test(filePath),  // save a copy of the manifest JSON file to disk, needed for webpack dev server
     },
-    devtool: 'cheap-module-source-map',
     output: {
         path: path.resolve(__dirname, 'build/dev_bundles/'),
         publicPath: 'http://localhost:7654/assets/bundles/'
     },
+    devtool: 'cheap-module-source-map',
     plugins: [
         // let Django know where bundles are
         // save the file one directory above from 'output.path'
@@ -28,25 +29,47 @@ module.exports = merge(common, {
         // delete all files in 'build/dev_bundles/' dir automatically
         new CleanWebpackPlugin(),
     ],
-});
+}
 
 
-/*
-*
-* Uncomment bellow to run local dev bundles, don't forget to comment above code
-* */
-// module.exports = merge(common, {
-//     devtool: 'cheap-module-source-map',
-//     output: {
-//         filename: "[name]-[chunkhash].js",
-//         path: path.resolve(__dirname, 'build/dev_bundles/'),
-//     },
-//     plugins: [
-//         // let Django know where bundles are
-//         // save the file one directory above from 'output.path'
-//         new WebpackManifestPlugin({fileName:'../manifest.dev.json', publicPath:'/dev_bundles/'}),
-//
-//         // delete all files in 'build/dev_bundles/' dir automatically
-//         new CleanWebpackPlugin(),
-//     ],
-// });
+// creates the local dev build files
+const devBuildConfig = {
+    output: {
+        filename: "[name]-[chunkhash].js",
+        path: path.resolve(__dirname, 'build/dev_bundles/'),
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: [
+        // let Django know where bundles are
+        // save the file one directory above from 'output.path'
+        new WebpackManifestPlugin({fileName:'../manifest.dev.json', publicPath:'/dev_bundles/'}),
+
+        // delete all files in 'build/dev_bundles/' dir automatically
+        new CleanWebpackPlugin(),
+    ],
+}
+
+
+module.exports = (env, args) => {
+    if(env.startDevServer) {
+        return merge(common, devServerConfig)
+    } else {
+        return merge(common, devBuildConfig)
+    }
+}
+
+
+
+
+// console.log('args:', process.argv)
+// process.argv[process.argv.length - 1]
+// module.exports = (env, args) => {
+//     console.log(env)
+//     console.log(args)
+//     switch (args.mode) {
+//         case 'development':
+//             return merge(common, {...});
+//         default:
+//             throw new Error('No matching configuration was found!');
+//     }
+// }
